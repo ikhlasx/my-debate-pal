dd4b7810e565d852a0ef9384800a0549ed07348a
ikhlasx <ikhlas110@gmail.com>
2026-01-20 10:13:43 +0530

    auth

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://your-issuer-url.clerk.accounts.dev",
+            domain: "https://sure-python-73.clerk.accounts.dev",
             applicationID: "convex",
         },
     ],
